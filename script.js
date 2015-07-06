0dafd4a97f0589b7c291e1cad4de87766d9d1439
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-07-06 13:06:49 -0700

    submit test

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('heimdall', [])
+  .constant("TEST", {
+    "API_URL": "https://shrouded-peak-6388.herokuapp.com/data"
+  })
+  .factory('Data', function($http, TEST) {
+    return {
+      getData: function() {
+        return $http.get(TEST.API_URL);
+      }
+    }
+  })
 
-.controller('MainCtrl', function($scope){
+.controller('MainCtrl', function($scope, Data) {
+  $scope.test = "highlight";
+  Data.getData().success(function(data) {
+    $scope.json = data.data;
+    $scope.singleObj = data.data[0];
+    console.log($scope.highlightclass);
+  });
 });
